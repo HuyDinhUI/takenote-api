d2038fda32a0cdc53b194be1b932ea6fa21ca102
HuyDinhUI <146050567+HuyDinhUI@users.noreply.github.com>
2026-01-31 21:25:36 +0700

    description type

--- src/modules/cards.js
+++ src/modules/cards.js
@@ -1,18 +1,33 @@
 import mongoose, { Schema } from "mongoose";
 
-const cardsSchema = new mongoose.Schema({
+const cardsSchema = new mongoose.Schema(
+  {
     label: { type: String, require: true },
     status: { type: Boolean, default: false },
-    columnId: { type: Schema.Types.ObjectId,ref:'Columns', require: true },
+    columnId: { type: Schema.Types.ObjectId, ref: "Columns", require: true },
     cover: { type: String },
-    description: { type: String },
+    description: {
+      time: { type: Number, require: true },
+      blocks: {
+        id: { type: String, require: true },
+        type: { type: String, require: true },
+        data: {
+          text: { type: String, require: true },
+          level: { type: Number, require: true },
+        },
+      },
+    },
     attechments: [{ type: String }],
-    checklist: [{
+    checklist: [
+      {
         label: { type: String, require: true },
-        checked: { type: Boolean, default: false }
-    }],
-    joined: [{type: Schema.Types.ObjectId, ref: 'Users'}],
-    _destroy: { type: Boolean, default: false }
-}, { timestamps: true })
+        checked: { type: Boolean, default: false },
+      },
+    ],
+    joined: [{ type: Schema.Types.ObjectId, ref: "Users" }],
+    _destroy: { type: Boolean, default: false },
+  },
+  { timestamps: true },
+);
 
-export const Card = mongoose.model('Cards', cardsSchema)
+export const Card = mongoose.model("Cards", cardsSchema);
